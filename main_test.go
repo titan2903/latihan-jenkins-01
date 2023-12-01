dae2bf9361c0cfb08d724367ec4c9f9c0d7effb6
titan <titanioy98@gmail.com>
2023-12-01 11:14:36 +0700

    add file main_test

---
+++ main_test.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestGetEnvHandler(t *testing.T) {
+	req, err := http.NewRequest("GET", "/getenv", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(getEnvHandler)
+
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("Handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
+
+	expectedContentType := "application/json"
+	if contentType := rr.Header().Get("Content-Type"); contentType != expectedContentType {
+		t.Errorf("Handler returned wrong content type: got %v want %v", contentType, expectedContentType)
+	}
+
+	// Add more assertions based on your specific requirements for the getEnvHandler
+}
+
+func TestHealthCheckHandler(t *testing.T) {
+	req, err := http.NewRequest("GET", "/health", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(healthCheckHandler)
+
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("Handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
+
+	expectedContentType := "application/json"
+	if contentType := rr.Header().Get("Content-Type"); contentType != expectedContentType {
+		t.Errorf("Handler returned wrong content type: got %v want %v", contentType, expectedContentType)
+	}
+
+	// Add more assertions based on your specific requirements for the healthCheckHandler
+}
